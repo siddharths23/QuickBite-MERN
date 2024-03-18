3f8d433ec5e07d922d04d652d194772e92d01ead
Siddharth Sankar <siddharthsankar23@gmail.com>
2024-03-17 23:33:15 -0700

    adding profile forms

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -12,6 +12,12 @@ const MobileNavLinks = () => {
       >
         User Profile
       </Link>
+      <Link
+        to="/manage-restaurant"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        Manage Restaurant
+      </Link>
       <Button
         onClick={() => logout()}
         className="flex items-center px-3 font-bold hover:bg-gray-500"
