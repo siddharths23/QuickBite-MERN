1024b64a47803bc3a496448b0077e2135b1cb1df
Siddharth Sankar <siddharthsankar23@gmail.com>
2024-03-19 23:31:43 -0700

    added search page & filters

---
+++ src/api/RestaurantApi.tsx
@@ -0,0 +1,35 @@
+import { SearchState } from "@/pages/SearchPage";
+import { RestaurantSearchResponse } from "@/types";
+import { useQuery } from "react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useSearchRestaurants = (
+  searchState: SearchState,
+  city?: string
+) => {
+  const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
+    const params = new URLSearchParams();
+    params.set("searchQuery", searchState.searchQuery);
+    params.set("page", searchState.page.toString());
+    params.set("selectedCuisines", searchState.selectedCuisines.join(","));
+    params.set("sortOption", searchState.sortOption);
+    const response = await fetch(
+      `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch restaurants");
+    }
+    return response.json();
+  };
+  const { data: results, isLoading } = useQuery(
+    ["searchRestaurants", searchState], //anytime searchQuery changes, execute query again
+    createSearchRequest,
+    { enabled: !!city } //query not going to run if city is undefined
+  );
+  return {
+    results,
+    isLoading,
+  };
+};
